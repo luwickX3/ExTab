58fbad1e28bb5a6c0f461ee30b0210243827eeef
luwickX3 <124098586+luwickX3@users.noreply.github.com>
2023-09-10 19:12:45 -0600

    Add files via upload

---
+++ ExcelTab/ExcelTab/Form1.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Private Conn As SqlConnection
+    Private Cmd As SqlCommand
+    Private Reader As SqlDataReader
+    Private Resultado As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Conn = New SqlConnection("Initial Catalog = ExTab ;Data Source = localhost;Integrated Security = True")
+        Cmd = Conn.CreateCommand
+        Cmd.CommandText = "Select"
+    End Sub
+End Class
